8ac0ac5fd2f804a109e3e659d159789776b48144
DmytroPetrov <43111390+DmytroPetrov@users.noreply.github.com>
2018-09-09 17:11:13 +0300

    Lab 1 Task 2

--- Labs.c
+++ Labs.c
@@ -2,11 +2,19 @@
 
 int main(){
  int a, m=3, n=2;
-
+ //task 1
  a= n++*m;
-
+ printf("n=%d\n",n);//checking n after the operation
  printf("%d\n", a);
- printf("%d",n);
 
+ printf("Task 2\n");//task 2
+ a= n++<m;
+ printf("n=%d\n",n);//checking n after the operation
+ printf("%d\n", a);
+
+ printf("Task 2\n");//task 3
+ a=m-- >m;
+ printf("m=%d\n",m);//checking m after the operation
+ printf("%d\n", a);
 return 0;
 }
